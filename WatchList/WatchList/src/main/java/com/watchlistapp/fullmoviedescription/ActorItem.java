4d7da684157f455c05d0b2e479d77bc0726b599c
veinhorn <mcklas@mail.ru>
2014-02-06 14:29:38 +0300

    Code cleaning.

--- WatchList/WatchList/src/main/java/com/watchlistapp/fullmoviedescription/ActorItem.java
+++ WatchList/WatchList/src/main/java/com/watchlistapp/fullmoviedescription/ActorItem.java
@@ -1,22 +1,10 @@
 package com.watchlistapp.fullmoviedescription;
 
-import android.graphics.Bitmap;
-
 /**
  * Created by VEINHORN on 07/01/14.
  */
 public class ActorItem extends Actor {
-    private Bitmap actorAvatar;
-
     public ActorItem() {
         super();
-    }
-
-    public Bitmap getActorAvatar() {
-        return actorAvatar;
-    }
-
-    public void setActorAvatar(Bitmap actorAvatar) {
-        this.actorAvatar = actorAvatar;
     }
 }
