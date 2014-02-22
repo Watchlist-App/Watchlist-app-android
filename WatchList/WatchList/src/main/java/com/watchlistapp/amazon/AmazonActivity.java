fab834a72701f6d69c6cca807198b16f29bfe1a1
veinhorn <mcklas@mail.ru>
2014-02-23 01:07:38 +0300

    Added design improvements to Amazon Activity.

--- WatchList/WatchList/src/main/java/com/watchlistapp/amazon/AmazonActivity.java
+++ WatchList/WatchList/src/main/java/com/watchlistapp/amazon/AmazonActivity.java
@@ -1,7 +1,10 @@
 package com.watchlistapp.amazon;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
 import android.widget.ListView;
 
 import com.watchlistapp.R;
@@ -19,7 +22,9 @@ public class AmazonActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amazon_list);
 
-        getSupportActionBar().setTitle(getIntent().getStringExtra("movieTitle"));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.actionbar_background_color)));
+        actionBar.setTitle(Html.fromHtml("<b><font color=\"#424242\">" + getIntent().getStringExtra("movieTitle") + " on Amazon" + "</font></b>"));
 
         listView = (ListView)findViewById(R.id.activity_amazon_list);
 
